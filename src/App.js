231e6b5d3963e2e91f07d14c64a5fa977f682e0f
iriscacais <mandeparairis@gmail.com>
2022-12-15 15:49:07 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Sidebar />
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
