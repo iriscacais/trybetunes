d6b843f899b46dca9f28880c8fbecd5fbb13ce29
iriscacais <mandeparairis@gmail.com>
2022-12-15 22:53:49 -0300

    requisito 3

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
+
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false, // definindo o estado de carregando como falso
+      name: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getUserName();
+  } // chamo a funçao ao carregar o componente header!
+
+  getUserName = async () => {
+    this.setState({ loading: true });
+    const userName = await getUser();
+    this.setState(() => ({
+      loading: false,
+      name: userName.name,
+    }));
+  }; // essa funcao de forma assincrona, enquanto espera o estado de loading muda para true, após a funcao getUser ser executada retorna o estado com loading falso e o valor username (input)
+
+  render() {
+    const { name, loading } = this.state;
+    if (loading) return <Loading />; // se o estado loading for true retorna o componete Loading. Se nao retorna o codigo abaixo!
+    return (
+      <header data-testid="header-component">
+        <h3 data-testid="header-user-name">
+          {name}
+        </h3>
+      </header>
+    );
+  }
+}
+export default Header;
