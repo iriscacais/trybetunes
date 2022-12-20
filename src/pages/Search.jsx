6277c2c1089b1890aaaca8aa13707498232ab1ff
iriscacais <mandeparairis@gmail.com>
2022-12-19 23:07:12 -0300

    requisito 6 incompleto

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,24 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
+      input: '',
+      loading: false,
       disabledSearchButton: true,
+      albuns: [],
     };
   }
 
   onInputSearch = ({ target }) => {
     const minValue = 2;
-    const { value } = target;
     this.setState({
-      disabledSearchButton: value.length < minValue,
+      input: target.value,
+      disabledSearchButton: target.value.length < minValue,
+    });
+  };
+
+  onSearchButtonClick = async (e) => {
+    e.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const { input } = this.state;
+    const albuns = await searchAlbumsAPI(input);
+    this.setState({
+      loading: false,
+      albuns,
     });
   };
 
   render() {
-    const { disabledSearchButton } = this.state;
+    const { albuns, disabledSearchButton, input, loading } = this.state;
     return (
       <>
         <div>
@@ -31,15 +50,49 @@ class Search extends React.Component {
               type="text"
               data-testid="search-artist-input"
               onChange={ this.onInputSearch }
+              value={ input }
             />
             <button
               type="button"
               data-testid="search-artist-button"
               disabled={ disabledSearchButton }
+              onClick={ this.onSearchButtonClick }
             >
               Pesquisar
             </button>
           </form>
+          {
+            loading ? <Loading /> : null // se loading for verdadeiro renderiza o componente Loading. Se nao permanece na página de login em que já estamos.
+          }
+          {
+            albuns.length > 0 && (
+              <p>
+                Resultado de álbuns de:
+                {' '}
+                {input}
+              </p>
+            )
+          }
+          {
+            albuns.length === 0 && <p> Nenhum álbum foi encontrado</p>
+          }
+          {
+            albuns.length > 0 && (
+              <ul>
+                {
+                  albuns.map(((album) => (
+                    <li key={ album.collectionId }>
+                      {album.collectionName}
+                      <Link
+                        to={ `/album/${album.collectionId}` }
+                        data-testid={ `link-to-album-${album.collectionId}` }
+                      />
+                    </li>
+                  )))
+                }
+              </ul>
+            )
+          }
         </div>
       </>
     );
